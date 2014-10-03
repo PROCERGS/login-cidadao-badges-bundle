cff55554cb610d2d191f660be6ef77c2116b5250
Guilherme Donato <guilhermednt@users.noreply.github.com>
2014-10-03 15:37:15 -0300

    Implemented Login Cidadão's badges evaluation

--- Event/BadgesSubscriber.php
+++ Event/BadgesSubscriber.php
@@ -11,17 +11,52 @@ class BadgesSubscriber extends AbstractBadgesEventSubscriber
 
     public function onBadgeEvaluate(EvaluateBadgesEvent $event)
     {
-        // Magic here
+        $this->checkCpf($event);
+        $this->checkEmail($event);
+        $this->checkNfg($event);
     }
 
     public function getName()
     {
-        return '';
+        return 'login-cidadao';
     }
 
     public function getAvailableBadges()
     {
         return array();
+    }
+
+    protected function checkCpf(EvaluateBadgesEvent $event)
+    {
+        $person = $event->getPerson();
+        if (is_numeric($person->getCpf()) && strlen($person->getNfgAccessToken()) > 0) {
+            $badge = new Badge($this->getName(), 'has_cpf', true);
+            $event->registerBadge($badge);
+        }
+    }
+
+    protected function checkEmail(EvaluateBadgesEvent $event)
+    {
+        $person = $event->getPerson();
+        if ($person->getEmailConfirmedAt() instanceof \DateTime && is_null($person->getConfirmationToken())) {
+            $badge = new Badge($this->getName(), 'valid_email', true);
+            $event->registerBadge($badge);
+        }
+    }
+
+    protected function checkNfg(EvaluateBadgesEvent $event)
+    {
+        $person = $event->getPerson();
+        if ($person->getNfgProfile()) {
+
+            $event->registerBadge(new Badge($this->getName(), 'nfg_access_lvl',
+                                            $person->getNfgProfile()->getAccessLvl()));
+
+            if ($person->getNfgProfile()->getVoterRegistrationSit() > 0) {
+                $event->registerBadge(new Badge($this->getName(),
+                                                'voter_registration', true));
+            }
+        }
     }
 
 }
