fcaccb4df3c42e28f7529af91d959f7b3ad6be56
Guilherme Donato <github@cadastro.dona.to>
2015-06-26 15:33:47 -0300

    Removed PROCERGS code

--- Event/BadgesSubscriber.php
+++ Event/BadgesSubscriber.php
@@ -12,7 +12,6 @@ use PROCERGS\LoginCidadao\BadgesControlBundle\Model\BadgeInterface;
 
 class BadgesSubscriber extends AbstractBadgesEventSubscriber
 {
-
     /** @var TranslatorInterface */
     protected $translator;
 
@@ -23,34 +22,23 @@ class BadgesSubscriber extends AbstractBadgesEventSubscriber
                                 EntityManager $em)
     {
         $this->translator = $translator;
-        $this->em = $em;
+        $this->em         = $em;
 
         $namespace = 'login-cidadao';
         $this->setName($namespace);
 
         $this->registerBadge('has_cpf',
-                             $translator->trans("$namespace.has_cpf.description",
-                                                array(), 'badges'),
-                                                array('counter' => 'countHasCpf'));
+            $translator->trans("$namespace.has_cpf.description", array(),
+                'badges'), array('counter' => 'countHasCpf'));
         $this->registerBadge('valid_email',
-                             $translator->trans("$namespace.valid_email.description",
-                                                array(), 'badges'),
-                                                array('counter' => 'countValidEmail'));
-        $this->registerBadge('nfg_access_lvl',
-                             $translator->trans("$namespace.nfg_access_lvl.description",
-                                                array(), 'badges'),
-                                                array('counter' => 'countNfgAccessLvl'));
-        $this->registerBadge('voter_registration',
-                             $translator->trans("$namespace.voter_registration.description",
-                                                array(), 'badges'),
-                                                array('counter' => 'countVoterRegistration'));
+            $translator->trans("$namespace.valid_email.description", array(),
+                'badges'), array('counter' => 'countValidEmail'));
     }
 
     public function onBadgeEvaluate(EvaluateBadgesEvent $event)
     {
         $this->checkCpf($event);
         $this->checkEmail($event);
-        $this->checkNfg($event);
     }
 
     public function onListBearers(ListBearersEvent $event)
@@ -58,14 +46,14 @@ class BadgesSubscriber extends AbstractBadgesEventSubscriber
         $filterBadge = $event->getBadge();
         if ($filterBadge instanceof BadgeInterface) {
             $countMethod = $this->badges[$filterBadge->getName()]['counter'];
-            $count = $this->{$countMethod}($filterBadge->getData());
+            $count       = $this->{$countMethod}($filterBadge->getData());
 
             $event->setCount($filterBadge, $count);
         } else {
             foreach ($this->badges as $name => $badge) {
                 $countMethod = $badge['counter'];
-                $count = $this->{$countMethod}();
-                $badge = new Badge($this->getName(), $name);
+                $count       = $this->{$countMethod}();
+                $badge       = new Badge($this->getName(), $name);
 
                 $event->setCount($badge, $count);
             }
@@ -75,7 +63,7 @@ class BadgesSubscriber extends AbstractBadgesEventSubscriber
     protected function checkCpf(EvaluateBadgesEvent $event)
     {
         $person = $event->getPerson();
-        if (is_numeric($person->getCpf()) && strlen($person->getNfgAccessToken()) > 0) {
+        if (is_numeric($person->getCpf())) {
             $event->registerBadge($this->getBadge('has_cpf', true));
         }
     }
@@ -85,20 +73,6 @@ class BadgesSubscriber extends AbstractBadgesEventSubscriber
         $person = $event->getPerson();
         if ($person->getEmailConfirmedAt() instanceof \DateTime && is_null($person->getConfirmationToken())) {
             $event->registerBadge($this->getBadge('valid_email', true));
-        }
-    }
-
-    protected function checkNfg(EvaluateBadgesEvent $event)
-    {
-        $person = $event->getPerson();
-        if ($person->getNfgProfile()) {
-
-            $event->registerBadge($this->getBadge('nfg_access_lvl',
-                                                  $person->getNfgProfile()->getAccessLvl()));
-
-            if ($person->getNfgProfile()->getVoterRegistrationSit() > 0) {
-                $event->registerBadge($this->getBadge('voter_registration', true));
-            }
         }
     }
 
@@ -117,7 +91,6 @@ class BadgesSubscriber extends AbstractBadgesEventSubscriber
                 ->createQueryBuilder('p')
                 ->select('COUNT(p)')
                 ->andWhere('p.cpf IS NOT NULL')
-                ->andWhere('p.nfgAccessToken IS NOT NULL')
                 ->getQuery()->getSingleScalarResult();
     }
 
@@ -130,50 +103,4 @@ class BadgesSubscriber extends AbstractBadgesEventSubscriber
                 ->andWhere('p.emailConfirmedAt IS NOT NULL')
                 ->getQuery()->getSingleScalarResult();
     }
-
-    protected function countNfgAccessLvl($filterData = null)
-    {
-        $empty = array(
-            "1" => 0,
-            "2" => 0,
-            "3" => 0
-        );
-
-        $query = $this->em->getRepository('PROCERGSLoginCidadaoCoreBundle:Person')
-            ->createQueryBuilder('p')
-            ->select('n.accessLvl, COUNT(n) total')
-            ->join('PROCERGSLoginCidadaoCoreBundle:NfgProfile', 'n', 'WITH',
-                   'p.nfgProfile = n')
-            ->groupBy('n.accessLvl');
-
-        if ($filterData !== null) {
-            $query->andWhere('n.accessLvl = :filterData')
-                ->setParameters(compact('filterData'));
-        }
-
-        $count = $query->getQuery()->getResult();
-        if (!empty($count)) {
-            $original = $count;
-            $count = $empty;
-            foreach ($original as $line) {
-                $level = $line['accessLvl'];
-                $count[$level] = $line['total'];
-            }
-            return $count;
-        } else {
-            return $empty;
-        }
-    }
-
-    protected function countVoterRegistration()
-    {
-        return $this->em->getRepository('PROCERGSLoginCidadaoCoreBundle:Person')
-                ->createQueryBuilder('p')
-                ->select('COUNT(p)')
-                ->join('PROCERGSLoginCidadaoCoreBundle:NfgProfile', 'n', 'WITH',
-                       'p.nfgProfile = n')
-                ->andWhere('n.voterRegistrationSit > 0')
-                ->getQuery()->getSingleScalarResult();
-    }
-
 }
